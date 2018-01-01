361ddc61d579f9a03d23d6a0f6c4253a24727cc9
eric-yoo <ajoije729@gmail.com>
2018-01-01 20:35:01 +0900

    180101 stats update

--- dog/static/js/stats.js
+++ dog/static/js/stats.js
@@ -1,53 +1,12 @@
-window.onload = function () {
-    $(".page").each(function () {
-        // 개별적으로 Wheel 이벤트 적용
-        $(this).on("mousewheel DOMMouseScroll", function (e) {
-            e.preventDefault();
-            var delta = 0;
-            if (!event) event = window.event;
-            if (event.wheelDelta) {
-                delta = event.wheelDelta / 120;
-                if (window.opera) delta = -delta;
-            } else if (event.detail) delta = -event.detail / 3;
-            var moveTop = null;
-                                
-           
-                                    // alert("!!!!");
-
-            // 마우스휠을 위에서 아래로
-            if (delta < 0) {
-                
-                if ($(this).next() != undefined) {
-                    var tmp=$(this).offset().top;
-                    moveTop = $(this).next().offset().top;
-                    if (moveTop==0){
-                        moveTop=tmp;
-                    }
-                    console.log(delta)
-
-                }
-
-            // 마우스휠을 아래에서 위로
-            } else {
-                if($(document).scrollTop()<800){
-                    moveTop=0;
-                }
-                else if ($(this).prev() != undefined) {
-                    moveTop = $(this).prev().offset().top;
-                }
-            }
-            // 화면 이동 0.8초(800)
-            $("html,body").stop().animate({
-                scrollTop: moveTop + 'px'
-            }, {
-                duration: 300, complete: function () {
-                }
-            });
-        });
-    });
+var mq = window.matchMedia( "(max-width: 780px)" );
+if (mq.matches) {
+    // window width is at less than 780px
 }
-
+else {
+    // window width is greater than 780px
+}
 window.onscroll = function() {scrollFunction()};
+
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -70,14 +29,14 @@ var flag3=0;
 
 $(window).scroll(function () {
 
-    /*  scroll 값  */
-    var height = $(document).scrollTop();
+	/*  scroll 값  */
+	var height = $(document).scrollTop();
 
-    /*  scroll 값 확인용 함수호출  */
-    log(height);
+	/*  scroll 값 확인용 함수호출  */
+	log(height);
 
 
-    /*  상단 고정 메뉴바 이벤트  */
+	/*  상단 고정 메뉴바 이벤트  */
                 if(height > 100){
                     if(flag3==0){
                         $('.top').css("background-color", "#B5996D");
@@ -148,77 +107,3 @@ $(window).scroll(function () {
 function log(str){
     $('#tmp').text(str);
 }
-
-var flag_img1=0;
-var flag_img2=0;
-var flag_img3=0;
-var flag_img4=0;
-
-
-
-$(document).ready(function(){
-    $("#showTruth1").click(function(){
-        if(flag_img1==0){
-            flag_img1=1;
-            $("#stat_img1_2").css("visibility", "visible");
-            $("#stat_img1_1").css("opacity", "0.5");
-            $(".page").css("background-color", "#242424");
-           
-        }
-        else{
-            flag_img1=0;
-            $("#stat_img1_2").css("visibility", "hidden");
-            $("#stat_img1_1").css("opacity", "1"); 
-            $(".page").css("background-color", "#F7F1E6");
-    }
-    });
-
-    $("#showTruth2").click(function(){
-        if(flag_img2==0){
-            flag_img2=1;
-            $("#stat_img2_2").css("visibility", "visible");
-            $("#stat_img2_1").css("opacity", "0.5");
-            $(".page").css("background-color", "#242424");
-           
-        }
-        else{
-            flag_img2=0;
-            $("#stat_img2_2").css("visibility", "hidden");
-            $("#stat_img2_1").css("opacity", "1"); 
-            $(".page").css("background-color", "#F7F1E6");
-    }
-    });
-
-    $("#showTruth3").click(function(){
-        if(flag_img3==0){
-            flag_img3=1;
-            $("#stat_img3_2").css("visibility", "visible");
-            $("#stat_img3_1").css("opacity", "0.5");
-            $(".page").css("background-color", "#242424");
-           
-        }
-        else{
-            flag_img3=0;
-            $("#stat_img3_2").css("visibility", "hidden");
-            $("#stat_img3_1").css("opacity", "1"); 
-            $(".page").css("background-color", "#F7F1E6");
-    }
-    });
-
-
-    $("#showTruth4").click(function(){
-        if(flag_img4==0){
-            flag_img4=1;
-            $("#stat_img4_2").css("visibility", "visible");
-            $("#stat_img4_1").css("opacity", "0.5");
-            $(".page").css("background-color", "#242424");
-           
-        }
-        else{
-            flag_img4=0;
-            $("#stat_img4_2").css("visibility", "hidden");
-            $("#stat_img4_1").css("opacity", "1"); 
-            $(".page").css("background-color", "#FFF5E6");
-    }
-    });
-});
